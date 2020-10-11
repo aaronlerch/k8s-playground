65e74a206ffcef306423df0b80573408107adc54
Aaron Lerch <aaronlerch@gmail.com>
2020-10-11 12:15:00 -0600

    Initialize sphere with secrets and lightstep and add a helm chart for deploying

--- services/sphere/app.rb
+++ services/sphere/app.rb
@@ -1,10 +1,34 @@
 require 'sinatra'
 require 'json'
 require 'net/http'
+require 'ddtrace'
 require_relative 'sphere'
+require_relative 'vault_secret_reader'
 
-set :seed_host, ENV['SEED_HOST'] || 'seed'
+set :seed_host, ENV['SEED_HOST'] || 'seed.seed.svc'
 set :seed_port, ENV['SEED_PORT'] || '80'
+
+secrets = VaultSecretReader.new("sphere").load
+if secrets.loaded?
+  envs = secrets.as_env
+  puts "Vault secrets loaded! Loaded #{envs.keys.length} secrets"
+  ENV.merge!(envs)
+end
+
+if ENV['SPHERE_LIGHTSTEP_TOKEN']
+  puts "Configuring Lightstep using the retrieved SPHERE_LIGHTSTEP_TOKEN"
+  Datadog.configure do |c|
+    c.use :sinatra, { service_name: 'k8s-playground-sphere' }
+
+    c.distributed_tracing.propagation_inject_style = [Datadog::Ext::DistributedTracing::PROPAGATION_STYLE_B3]
+    c.distributed_tracing.propagation_extract_style = [Datadog::Ext::DistributedTracing::PROPAGATION_STYLE_B3]
+
+    c.tracer tags: {
+      'lightstep.service_name' => 'k8s-playground-sphere',
+      'lightstep.access_token' => ENV['SPHERE_LIGHTSTEP_TOKEN']
+    }
+  end
+end
 
 get '/' do
   'Hello sphere!'
